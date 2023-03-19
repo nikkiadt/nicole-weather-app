f8012938297ec75367102ed7537a3d5cf7175816
Nicole Tan <63406516+nikkiadt@users.noreply.github.com>
2023-03-19 13:09:52 +0200

    Cleaned code, changed API, updated styling
    Added containers to index.html, changed from Open Weather to SheCodes Weather API, updated some CSS styling.

--- script.js
+++ script.js
@@ -1,67 +1,46 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-function getHours2Digits() {
-  return String(now.getHours()).padStart(2, "0");
-}
-let hour = getHours2Digits(now);
-function getMinutes2Digits() {
-  return String(now.getMinutes()).padStart(2, "0");
-  mainte;
-}
-let minute = getMinutes2Digits(now);
-let date = document.querySelector("#date");
-date.innerHTML = `${day} ${hour}:${minute}`;
+let apiKey = "cbe4bc31a43ff3543at23227e11o06d3";
+let units = "metric";
 
-let weatherForm = document.querySelector("#weather-form");
-weatherForm.addEventListener("submit", handleSubmit);
+function formatDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function displayWeatherConditions(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#main-temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#city-name").innerHTML = response.data.city;
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
 }
 
-function searchCity(city) {
-  let apiKey = "4eb0104eed7720d373d92522833cf75b";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeatherConditions);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.getElementById("search-input").value;
-  searchCity(city);
-}
-
-let locationButton = document.querySelector("#current-location");
-locationButton.addEventListener("click", getCurrentLocation);
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(userPosition);
-}
-
-function userPosition(position) {
-  let apiKey = "4eb0104eed7720d373d92522833cf75b";
-  let units = "metric";
-  let positionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${units}&appid=${apiKey}`;
-  axios.get(positionUrl).then(displayWeatherConditions);
-}
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query="Paris"&key=${apiKey}`;
+axios.get(apiUrl).then(displayWeatherConditions);
